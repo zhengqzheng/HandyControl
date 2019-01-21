13d7ce2097e1dbe463767425b81ed96c32d5a902
Mahdi Hosseini <mahdidvb72@gmail.com>
2019-01-21 10:03:59 +0330

    fix test

--- HandyControlDemo/MainWindow.xaml.cs
+++ HandyControlDemo/MainWindow.xaml.cs
@@ -1,10 +1,29 @@
-﻿namespace HandyControlDemo
+﻿using HandyControl.Data;
+using Microsoft.Win32;
+using System;
+using System.Linq;
+using System.Management;
+using System.Windows;
+
+namespace HandyControlDemo
 {
     public partial class MainWindow
     {
         public MainWindow()
         {
             InitializeComponent();
+
+            RegistryKey registryKey = Registry.LocalMachine.OpenSubKey(@"SOFTWARE\Microsoft\Windows NT\CurrentVersion");
+
+            var buildNumber = Convert.ToInt32(registryKey.GetValue("CurrentBuild"));
+            var buildNumber2 = Convert.ToInt32(registryKey.GetValue("CurrentMajorVersionNumber"));
+            var buildNumber3 = Convert.ToInt32(registryKey.GetValue("CurrentMinorVersionNumber"));
+
+            var info = new SystemVersionInfo(buildNumber2, buildNumber3, buildNumber);
+            
+
+            MessageBox.Show(info.Major + " " + info.Minor + " " + info.Build);
         }
+        
     }
 }
